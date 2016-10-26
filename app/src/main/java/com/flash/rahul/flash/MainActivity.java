493ac6b6132a65ec805f09e7ba519142559e5a2f
rahulworld <rahulnitsxr@gmail.com>
2016-10-26 13:04:34 +0530

    Added Flash

--- app/src/main/java/com/flash/rahul/flash/MainActivity.java
+++ app/src/main/java/com/flash/rahul/flash/MainActivity.java
@@ -1,6 +1,7 @@
 package com.flash.rahul.flash;
 
 
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -23,10 +24,120 @@ public class MainActivity extends AppCompatActivity {
     private Boolean isTorchOn;
     private MediaPlayer mp;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTorchOnOffButton = (ImageButton) findViewById(R.id.button_on_off);
+        isTorchOn = false;
+        Boolean isFlashAvailable = getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
 
+        if (!isFlashAvailable) {
+
+            AlertDialog alert = new AlertDialog.Builder(MainActivity.this)
+                    .create();
+            alert.setTitle("Error !!");
+            alert.setMessage("Your device doesn't support flash light!");
+            alert.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // closing the application
+                    finish();
+                    System.exit(0);
+                }
+            });
+            alert.show();
+            return;
+        }
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            mCameraId = mCameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+        mTorchOnOffButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    if (isTorchOn) {
+                        turnOffFlashLight();
+                        isTorchOn = false;
+                    } else {
+                        turnOnFlashLight();
+                        isTorchOn = true;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void turnOnFlashLight() {
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mCameraManager.setTorchMode(mCameraId, true);
+                playOnOffSound();
+                mTorchOnOffButton.setImageResource(R.drawable.button_on);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void turnOffFlashLight() {
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mCameraManager.setTorchMode(mCameraId, false);
+                playOnOffSound();
+                mTorchOnOffButton.setImageResource(R.drawable.button_on);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void playOnOffSound(){
+
+        mp = MediaPlayer.create(MainActivity.this, R.raw.light_switch_off);
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                // TODO Auto-generated method stub
+                mp.release();
+            }
+        });
+        mp.start();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(isTorchOn){
+            turnOffFlashLight();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(isTorchOn){
+            turnOffFlashLight();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(isTorchOn){
+            turnOnFlashLight();
+        }
     }
 }
